764f6c1607f241aa72e5dd5dcfdc828a9b6ae2a4
Hung Tran <hung.tran@swissitbridge.com>
2018-07-11 13:43:11 +0700

    comments

--- src/app/dynamic-form/control/textbox.ts
+++ src/app/dynamic-form/control/textbox.ts
@@ -1,11 +1,12 @@
 import { BaseControl } from './base-control';
 
 export class Textbox extends BaseControl<string> {
+    // this tells which control type should be displayed when we have a list of controls (in dynamic-form-control.component)
     type = 'textbox';
-    inputType: string;
+    inputType: string; // e.g. 'text', 'number', 'email',...
 
     constructor(options: {} = {}) {
-        super(options);
+        super(options); // pass the options argument into the constructor of the BaseControl class
         this.inputType = options['inputType'] || '';
     }
 }
