2dd6c4f3f03102876b5021e749ba6a45ce732521
ht89 <hungtran891989@gmail.com>
2018-07-17 09:31:15 +0700

    commit

--- src/app/dynamic-form/control/control.service.ts
+++ src/app/dynamic-form/control/control.service.ts
@@ -7,19 +7,19 @@ export class ControlService {
 
     constructor(private fb: FormBuilder) { }
 
-    /*
-        convert all controls into FormControls & store them in a FormGroup
+    
+        // convert all controls into FormControls & store them in a FormGroup
 
-        a normal form is defined like this:
+        // a normal form is defined like this:
 
-        this.myForm = this.fb.group({
-            name: ['', [Validators.required, Validators.minLength(5)]],
-            address: this.fb.group({
-                street: ['', <any>Validators.required],
-                postcode: ['']
-            })
-        });
-    */
+        // this.myForm = this.fb.group({
+        //     name: ['', [Validators.required, Validators.minLength(5)]],
+        //     address: this.fb.group({
+        //         street: ['', <any>Validators.required],
+        //         postcode: ['']
+        //     })
+        // });
+    
 
     toFormGroup(controls: BaseControl<any>[]) {
         const group: any = [];
